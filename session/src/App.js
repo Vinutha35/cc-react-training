a9db63ad42947e307c5dadfd06a939a02fb86178
Vinutha35 <90311549+Vinutha35@users.noreply.github.com>
2021-10-04 13:04:16 +0530

    Add files via upload

---
+++ session/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Listoffruits from './components/Listoffruits';
+
+function App() {
+  return (
+    <div className="container">
+    <Listoffruits/>
+     </div>
+  );
+}
+export default App;
+
+
